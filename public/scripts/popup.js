75a7ae85c973b09b5626984957a88469ab8086a1
TimProger <akylverentimofei@mail.ru>
2021-12-12 04:08:42 +0300

    Хэдэр доделан. Попап попапается, но всё ещё на стадии разработки.

--- public/scripts/popup.js
+++ public/scripts/popup.js
@@ -1,18 +1,22 @@
 const catalogue = document.querySelector('a[href="/catalogue"]');
-const catalogue_popup = document.querySelector('.catalogue__popup');
+const popup = document.querySelector('.popup');
 
 catalogue.addEventListener('click', (evt) => {
     evt.preventDefault();
     catalogue.classList.toggle('active');
     if (catalogue.classList.contains('active')) {
-        catalogue_popup.style = `
+        popup.style = `
         visibility: visible;
         opacity: 1;
-        top: ${catalogue.offsetHeight}px;                   
-        left: ${catalogue.offsetLeft}px;`;
+        top: 50px;  
+        `;
+        // top: ${catalogue.offsetHeight}px;                   
+        // left: ${catalogue.offsetLeft}px;
     } else if (!catalogue.classList.contains('active')) {
-        catalogue_popup.style = `
+        popup.style = `
         visibility: hidden;
-        opacity: 0;`;
+        opacity: 0;
+        top: 30px; 
+        `;
     }
 });
--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,15 +1,64 @@
 const router = require("express").Router();
 
 const links = [
-    { name: "Каталог", src: "/catalogue" },
-    { name: "Контакты", src: "/contacts" },
-    { name: "Профиль", src: "/profile" }
+    { name: "Каталог", url: "/catalogue" },
+    { name: "Контакты", url: "/contacts" }
+];
+
+// const catalogue_tabs = [
+//     { name: "Ролевые игры", url: "/catalogue/:rpgames" },
+//     { name: "ККИ", url: "/catalogue/:ccg" },
+//     { name: "Warhammer", url: "/catalogue/:warhammer" },
+//     { name: "Пазлы и гловоломки", url: "/catalogue/:puzzles" },
+//     { name: "Книги и комиксы", url: "/catalogue/:bc" },
+//     { name: "Сборные модели", url: "/catalogue/:models" }
+// ];
+
+const catalogue_tabs = [
+    {
+        name: "Warhammer", url: "/catalogue/:warhammer", tabs: [
+            { name: "Warhammer 40000", url: "/catalogue/:warhammer?p=w4k" },
+            { name: "Warhammer Age of Sigmar", url: "/catalogue/:warhammer?p=waof" },
+            { name: "Коробочные Игры", url: "/catalogue/:warhammer?p=bg" },
+            { name: "Краски Citadel Colour", url: "/catalogue/:warhammer?p=ccc" }
+        ]
+    },
+    {
+        name: "ККИ", url: "/catalogue/:ccg", tabs: [
+            { name: "MTG", url: "/catalogue/:ccg?p=mtg" },
+            { name: "Gwent", url: "/catalogue/:ccg?p=gwent" },
+            { name: "Берсерк. Герои", url: "/catalogue/:ccg?p=berserk" },
+            { name: "Star Wars: Destiny", url: "/catalogue/:ccg?p=swd" }
+        ]
+    },
+    {
+        name: "Ролевые игры", url: "/catalogue/:rpgames", tabs: [
+            { name: "Dungeons&Dragons", url: "/catalogue/:rpgames?p=dnd" },
+            { name: "Pathfinder", url: "/catalogue/:rpgames?p=pf" },
+            { name: "Кориолис", url: "/catalogue/:rpgames?p=kor" },
+            { name: "Game of Thrones", url: "/catalogue/:rpgames?p=got" }
+        ]
+    },
+    {
+        name: "Пазлы и гловоломки", url: "/catalogue/:puzzles", tabs: [
+            { name: "Пазлы", url: "/catalogue/:puzzles?p=puzzles" },
+            { name: "Головоломки", url: "/catalogue/:puzzles?p=brainteaser" }
+        ]
+    },
+    {
+        name: "Сборные модели", url: "/catalogue/:models", tabs: [
+            { name: "Техника", url: "/catalogue/:models?p=tech" },
+            { name: "Авиация", url: "/catalogue/:models?p=aviation" },
+            { name: "Автомобили", url: "/catalogue/:models?p=cars" }
+        ]
+    }
 ];
 
 router.get("/", (req, res) => {
     res.render("index", {
         title: "Настольные игры",
         links: links,
+        catalogue_tabs: catalogue_tabs,
         class: "dark"
     });
 });
@@ -17,21 +66,24 @@ router.get("/", (req, res) => {
 router.get("/basket", (req, res) => {
     res.render("basket", {
         title: "Корзина",
-        links: links
+        links: links,
+        catalogue_tabs: catalogue_tabs
     });
 });
 
 router.get("/profile", (req, res) => {
     res.render("profile", {
         title: "Профиль",
-        links: links
+        links: links,
+        catalogue_tabs: catalogue_tabs
     });
 });
 
 router.get("/catalogue", (req, res) => {
     res.render("catalogue", {
         title: "Каталог",
-        links: links
+        links: links,
+        catalogue_tabs: catalogue_tabs
     });
 });
 
@@ -39,28 +91,32 @@ router.get('/catalogue/:name', (req, res) => {
     res.render('catalogue', {
         title: 'Каталог',
         caption: req.params.name,
-        links: links
+        links: links,
+        catalogue_tabs: catalogue_tabs
     });
 });
 
 router.get("/contacts", (req, res) => {
     res.render("index", {
         title: "Контакты",
-        links: links
+        links: links,
+        catalogue_tabs: catalogue_tabs
     });
 });
 
 router.get("/profile", (req, res) => {
     res.render("index", {
         title: "Профиль",
-        links: links
+        links: links,
+        catalogue_tabs: catalogue_tabs
     });
 });
 
 router.get("/basket", (req, res) => {
     res.render("index", {
         title: "Корзина",
-        links: links
+        links: links,
+        catalogue_tabs: catalogue_tabs
     });
 });
 
@@ -69,7 +125,8 @@ router.get("*", (req, res) => {
     res.render('error', {
         title: 'Error',
         caption: 'Ошибка, данный запрос не существует',
-        links: links
+        links: links,
+        catalogue_tabs: catalogue_tabs
     });
 });
 
