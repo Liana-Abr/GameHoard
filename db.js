40714fc3a2bfaed1b7202fb1793205068f94000e
TimProger <akylverentimofei@mail.ru>
2021-12-30 00:27:08 +0300

    sql

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-const devConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-};
+// const devConfig = {
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME
+// };
 // const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@
 // ${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
+"pg://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/" + process.env.DB_NAME + "?ssl=true";
 const proConfig = {
     connectionString: process.env.HEROKU_POSTGRESQL_DBNAME_URL,
     ssl: true
