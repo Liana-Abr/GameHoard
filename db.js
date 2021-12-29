2d969cff758de2f06d31eb81db8709266368601b
TimProger <akylverentimofei@mail.ru>
2021-12-29 23:07:12 +0300

    Добавил файл sql и подгтовил к деплою на хероку

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
 const Pool = require('pg').Pool;
-const pool = new Pool({
+require('dotenv').config();
+
+const devConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
-});
+};
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL
+};
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
 module.exports = pool;
